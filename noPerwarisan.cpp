39ec48598d4d30eb640e7140785c67fd31b43a84
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-06-02 18:47:11 +0700

    perbaikan pewarisan class

---
+++ noPerwarisan.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Halo saya Function dari base class";
+    }
+};
+
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Halo saya Function dari derived Class";
+    }
+};
+
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
