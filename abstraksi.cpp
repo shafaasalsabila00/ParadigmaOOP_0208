a8a43e0e175249905516b0591ab0f5896ec0aca7
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-06-02 18:14:54 +0700

    menambahkan method setXY

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 class AbstraksiKlas
 {
-    
-}
+private:
+    string x, y;
+
+public:
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b)
+};
